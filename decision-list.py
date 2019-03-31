54d489646dc9f3bbb9826f3c0e14ffccad62012d
bjpranav <bjpranav@gmail.com>
2019-03-31 18:28:45 -0400

    Update decision-list.py

--- decision-list.py
+++ decision-list.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 30 18:40:00 2019
 
-@author: Pranav Krishna
-"""
 #Importing required packages
 import xml.etree.ElementTree as ET
+import numpy as np
+import pandas as pd
 import re
 import nltk
 
@@ -17,21 +14,76 @@ line=root.find('lexelt')
 
 #Returns two words before and after the ambigus word
 def bigram(xmlstr):
-    features=re.findall('(\w+) (\w+) <head>\w{4,5}<\/head> (\w+) (\w+)', str(xmlstr), re.IGNORECASE)
+    features=re.findall('(\w+ \w+)"?.?-? <head>\w{4,5}<\/head> (\w+ \w+)', str(xmlstr), re.IGNORECASE)
+    if(features==[]):
+        return('EOL')
+    else:
+        return(features)
+
+def kwordsBefore(xmlstr,k):
+    features=[]
+    cleanedWords = str(xmlstr).replace('</s>','').replace('<s>','').replace('</context>','').replace('\n','').replace('<context>','').replace('\\n\\n','').replace('\\n','').replace('.',' ')     
+    bag_of_words=cleanedWords.split()
+    
+    if('<head>line</head>' in cleanedWords):
+        index=bag_of_words.index('<head>line</head>')
+    elif('<head>lines</head>' in bag_of_words):
+        index=bag_of_words.index('<head>lines</head>')
+    elif('<head>Lines</head>' in bag_of_words):
+        index=bag_of_words.index('<head>Lines</head>')
+    elif('<head>Line</head>' in bag_of_words):
+        index=bag_of_words.index('<head>Line</head>')
+    else:
+        print(cleanedWords)
+    for i in range(1,k+1):
+        features.append(bag_of_words[index-i])
+    return(features)
+    
+def kwordsAfter(xmlstr,k):
+    features=[]
+    cleanedWords = str(xmlstr).replace('</s>','').replace('<s>','').replace('</context>','').replace('\n','').replace('<context>','').replace('\\n\\n','').replace('\\n','').replace('.',' ')     
+    bag_of_words=cleanedWords.split()
+    
+    if('<head>line</head>' in cleanedWords):
+        index=bag_of_words.index('<head>line</head>')
+    elif('<head>lines</head>' in bag_of_words):
+        index=bag_of_words.index('<head>lines</head>')
+    elif('<head>Lines</head>' in bag_of_words):
+        index=bag_of_words.index('<head>Lines</head>')
+    elif('<head>Line</head>' in bag_of_words):
+        index=bag_of_words.index('<head>Line</head>')
+    else:
+        print(cleanedWords)
+    for i in range(1,k+1):
+        if(len(bag_of_words)>index+i):
+            features.append(bag_of_words[index+i])
+        else:
+            features.append('EOL')
     return(features)
 
 #Returns a word before and after the ambigus word
 def unigram(xmlstr):
-    features=re.findall('(\w+) <head>\w{4,5}<\/head> (\w+)', str(xmlstr), re.IGNORECASE)
+    features=re.findall('(\w+)"?.?-? <head>\w{4,5}<\/head> (\w+)', str(xmlstr), re.IGNORECASE)
+    if(features==[]):
+        return(['EOL'])
+    else:
+        return(features)
     return(features)
 #Returns a word before the ambigus word
 def kminus1(xmlstr):
-    features=re.findall('(\w+) <head>', str(xmlstr), re.IGNORECASE)
+    features=re.findall('"?(\w+)"?.?-? <head>', str(xmlstr), re.IGNORECASE)
+    if(features==[]):
+        return(['EOL'])
+    else:
+        return(features)
     return(features)
 #Returns a word after the ambigus word
 def kplus1(xmlstr):
-    features=re.findall('</head> (\w+)', str(xmlstr), re.IGNORECASE)
-    return(features)
+    features=re.findall(r'</head> [,.;"]? ?(\w+)', str(xmlstr), re.IGNORECASE)
+    if(features==[]):
+        return(['EOL'])
+    else:
+        return(features)
 
 #Tags POS for all the words and returns the tag of the words before and after the 
 #ambiguous word
@@ -52,7 +104,17 @@ def tagger(xmlstr):
         return([beforeTag,afterTag])
     else:
         return(['NN','NN'])
-    
+        
+        
+beforeWord=[]
+afterWord=[]
+unigramFeature=[]
+bigramFeature=[]
+beforeTag=[]
+afterTag=[]
+senses=[]
+kminus2Feature=[]
+kplus2Feature=[]
 #Iterates through all the instances
 for val in line:
     #Stores the instance ID
@@ -62,21 +124,55 @@ for val in line:
         #If the tag has sense id the that is stored in the senseId variable
         if((context.attrib)!={}):
             senseId=(context.attrib)['senseid']
+            senses.append(senseId)
         #If the tag contains the string and the ambiguous word, fowwing are performed
         else:
             #Converts the tags into strings
             xmlstr = ET.tostring(context)
             #Gets the word before the ambigus word
-            beforeWord=kminus1(xmlstr)
+            beforeWord.append(((kminus1(xmlstr))[0],senseId))
             #Gets the word after the ambigus word
-            afterWord=kplus1(xmlstr)
+            afterWord.append(((kplus1(xmlstr))[0],senseId))
             #Gets a word before and after the ambigus word
-            unigramFeature=unigram(xmlstr)
+            unigramString=' '.join((unigram(xmlstr))[0])
+            unigramFeature.append((unigramString,senseId))
             #Gets two words before and after the ambigus word
-            bigramFeature=unigram(xmlstr)
+            bigramString=' '.join((bigram(xmlstr))[0])
+            bigramFeature.append((bigramString,senseId))
             # Gets the tag before and after the ambiguous tag
             tags=tagger(xmlstr)
-            beforeTag=tags[0]
-            afterTag=tags[1]
-            print(beforeTag,afterTag)
+            beforeTag.append((tags[0],senseId))
+            afterTag.append((tags[1],senseId))
+            #print(beforeTag,afterTag)
+            kminus2Feature.append((' '.join(kwordsBefore(xmlstr,2)),senseId))
+            kplus2Feature.append((' '.join(kwordsAfter(xmlstr,2)),senseId))
+            print(kwordsBefore(xmlstr,3))
 
+
+def setz(sequence):
+    sequenceList=[]
+    for i in sequence:
+        if(i[0] not in sequenceList):
+            sequenceList.append(i[0])
+    return(sequenceList)
+
+
+
+def initialize(sequence1,sequence2):
+    return(np.zeros((len(sequence1), len(sequence2))))
+    
+def dfBuilder(featureSequence):
+    uniqueList=setz(featureSequence)
+    initializeSet = initialize((uniqueList), set(senses))
+    initializedFeature=pd.DataFrame(initializeSet, index=uniqueList, columns=set(senses))
+    for i in featureSequence:
+        initializedFeature.at[i[0], i[1]]=initializedFeature.at[i[0], i[1]] + 1
+    return(initializedFeature)
+
+
+oneWordBefore=dfBuilder(beforeWord)
+oneWordAfter=dfBuilder(afterWord)
+unigramwords=dfBuilder(unigramFeature)
+bigramwords=dfBuilder(bigramFeature) 
+kminus2words=dfBuilder(kminus2Feature)
+kplus2words=dfBuilder(kplus2Feature)
